1d6c29aed6a8e608db69e1d10b6c62ab40e2c25a
Silvia Velicu <odileww@gmail.com>
2023-11-20 20:37:33 +0200

    Install json-server package and use it to setup fake api for stock; replace static mock with mock api.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { registerLocaleData } from '@angular/common';
 import * as de from '@angular/common/locales/de';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 registerLocaleData(de.default);
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
+        provideHttpClient(),
         {
             provide: LOCALE_ID,
             useValue: 'de-DE',
--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { products } from '../../assets/products';
 import { IProduct } from '../interfaces/product.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PaymentService {
+    private apiUrl = '/api/products';
+
     //coins balance
     private balanceSubject$: BehaviorSubject<number> = new BehaviorSubject(0);
     public balanceObservable: Observable<number> = this.balanceSubject$.asObservable();
 
     //products state
-    private productsSubject$: BehaviorSubject<IProduct[]> = new BehaviorSubject(products);
+    private productsSubject$: BehaviorSubject<IProduct[]>;
+
+    constructor(private httpClient: HttpClient) {}
 
     public updateBalance(amount: number): void {
         this.balanceSubject$.next(amount);
     }
 
     public getProducts(): Observable<IProduct[]> {
-        return this.productsSubject$;
+        return this.httpClient.get<IProduct[]>(this.apiUrl);
     }
 
     public updateStock(items: IProduct[]) {
